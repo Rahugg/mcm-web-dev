6bfb9ffaed5c862b22e59bd934bddc014e49a95f
Rahugg <rahugg07@gmail.com>
2023-11-16 20:47:06 +0600

    loader

---
+++ Lab11/loader/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("loading-text").innerText = "Loaded!";
+        document.querySelector(".loader-container").style.display = "none";
+        
+        addContent();
+    }, 3000); 
+    function addContent() {
+        var newParagraph = document.createElement("p");
+        newParagraph.innerText = "Content loaded successfully!";
+        
+        document.body.appendChild(newParagraph);
+    }
+});
